050fe796377bb636ed178ba5d23b239664f1f9e4
Ju <juliette.jeannin@hotmail.fr>
2023-04-21 10:39:12 +0200

    add Bernouilli law

--- main.js
+++ main.js
@@ -1,17 +1,28 @@
-// Variables
 let nb_tirage = 100;
-let random = Math.random();
+let random;
 let parameter = 0.6;
 
-// Main
+/* --- Bernouilli --- */
+random = Math.random();
 console.log(random);
-let geo_number = geometric(parameter, random);
-let moyenne = moyGeo(parameter, random, nb_tirage);
-let esperance = esperanceGeo(parameter);
-let variance = varianceGeo(parameter);
 
-// Affichage
+let bern_number = bernouilli(parameter, random);
+let moy_bern = moyBern(parameter, random, nb_tirage);
+
+showResult(bern_number);
+console.log("X value : " + bern_number);
+console.log("Moyenne : " + moy_bern);
+
+/* --- Geometric --- */
+random = Math.random();
+console.log(random);
+
+let geo_number = geometric(parameter, random);
+let moy_geo = moyGeo(parameter, random, nb_tirage);
+let esp_geo = esperanceGeo(parameter);
+let variance_geo = varianceGeo(parameter);
+
 console.log("X value : " + geo_number);
-console.log("Moyenne : " + moyenne);
-console.log("Espérance : " + esperance);
-console.log("Variance : " + variance);
+console.log("Moyenne : " + moy_geo);
+console.log("Espérance : " + esp_geo);
+console.log("Variance : " + variance_geo);
