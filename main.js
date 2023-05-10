659778265bab68edc0aace9766eed7b96f4f5c84
Ju <juliette.jeannin@hotmail.fr>
2023-05-10 14:29:29 +0200

    add uniform law

--- main.js
+++ main.js
@@ -1,28 +1,52 @@
-let nb_tirage = 100;
+let nb_tirages = 100;
 let random;
-let parameter = 0.6;
+let parametre = 0.6;
 
 /* --- Bernouilli --- */
+console.log("/* BERNOUILLI */");
+
 random = Math.random();
 console.log(random);
 
-let bern_number = bernouilli(parameter, random);
-let moy_bern = moyBern(parameter, random, nb_tirage);
+let nb_bern = bernouilli(parametre, random);
+let moy_bern = moyBern(parametre, random, nb_tirages);
+let esp_bern = esperanceBern(parametre);
+let var_bern = varianceBern(parametre);
 
-showResult(bern_number);
-console.log("X value : " + bern_number);
+afficheResultat(nb_bern);
+console.log("X : " + nb_bern);
 console.log("Moyenne : " + moy_bern);
+console.log("Espérance : " + esp_bern);
+console.log("Variance : " + var_bern);
 
-/* --- Geometric --- */
+/* --- Geometrique --- */
+console.log("/* GEOMETRIQUE */");
+
 random = Math.random();
 console.log(random);
 
-let geo_number = geometric(parameter, random);
-let moy_geo = moyGeo(parameter, random, nb_tirage);
-let esp_geo = esperanceGeo(parameter);
-let variance_geo = varianceGeo(parameter);
+let nb_geo = geometrique(parametre, random);
+let moy_geo = moyGeo(parametre, random, nb_tirages);
+let esp_geo = esperanceGeo(parametre);
+let var_geo = varianceGeo(parametre);
 
-console.log("X value : " + geo_number);
+console.log("X : " + nb_geo);
 console.log("Moyenne : " + moy_geo);
 console.log("Espérance : " + esp_geo);
-console.log("Variance : " + variance_geo);
+console.log("Variance : " + var_geo);
+
+/* --- Uniforme --- */
+console.log("/* UNIFORME */");
+
+entier_random = Math.floor(Math.random()*10);
+
+let nb_unif = uniforme(entier_random);
+let moy_unif = moyUniforme(entier_random);
+let esp_unif = esperanceUniforme(entier_random);
+let var_unif = varianceUniforme(entier_random);
+
+console.log("X : " + entier_random);
+console.log("Px : " + entier_random);
+console.log("Moyenne : " + moy_unif);
+console.log("Espérance : " + esp_unif);
+console.log("Variance : " + var_unif);
