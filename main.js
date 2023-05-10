3001a83986875e0fcd9cbafb8cf4f6f12c8e17fa
Ju <juliette.jeannin@hotmail.fr>
2023-05-10 15:04:27 +0200

    add binomial law + ecart-type for each lax

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 let nb_tirages = 100;
 let random;
+let entier_random;
 let parametre = 0.6;
 
 /* --- Bernouilli --- */
@@ -12,12 +13,14 @@ let nb_bern = bernouilli(parametre, random);
 let moy_bern = moyBern(parametre, random, nb_tirages);
 let esp_bern = esperanceBern(parametre);
 let var_bern = varianceBern(parametre);
+let ecart_type_bern = ecartTypeBern(parametre);
 
 afficheResultat(nb_bern);
 console.log("X : " + nb_bern);
 console.log("Moyenne : " + moy_bern);
 console.log("Espérance : " + esp_bern);
 console.log("Variance : " + var_bern);
+console.log("Ecart-type : " + ecart_type_bern);
 
 /* --- Geometrique --- */
 console.log("/* GEOMETRIQUE */");
@@ -29,11 +32,14 @@ let nb_geo = geometrique(parametre, random);
 let moy_geo = moyGeo(parametre, random, nb_tirages);
 let esp_geo = esperanceGeo(parametre);
 let var_geo = varianceGeo(parametre);
+let ecart_type_geo = ecartTypeBern(parametre);
 
 console.log("X : " + nb_geo);
 console.log("Moyenne : " + moy_geo);
 console.log("Espérance : " + esp_geo);
 console.log("Variance : " + var_geo);
+console.log("Ecart-type : " + ecart_type_geo);
+
 
 /* --- Uniforme --- */
 console.log("/* UNIFORME */");
@@ -44,9 +50,27 @@ let nb_unif = uniforme(entier_random);
 let moy_unif = moyUniforme(entier_random);
 let esp_unif = esperanceUniforme(entier_random);
 let var_unif = varianceUniforme(entier_random);
+let ecart_type_unif = ecartTypeUniforme(entier_random);
 
 console.log("X : " + entier_random);
 console.log("Px : " + entier_random);
 console.log("Moyenne : " + moy_unif);
 console.log("Espérance : " + esp_unif);
 console.log("Variance : " + var_unif);
+console.log("Ecart-type : " + ecart_type_unif);
+
+/* --- Binomiale --- */
+console.log("/* BINOMIALE */");
+
+random = Math.random();
+entier_random = Math.floor(Math.random()*10);
+
+let nb_bin = binomiale(entier_random, parametre, random);
+let esp_bin = esperanceBin(entier_random, parametre);
+let var_bin = varianceBin(entier_random, parametre);
+let ecart_type_bin = ecartTypeBin(entier_random, parametre);
+
+console.log("X : " + entier_random);
+console.log("Espérance : " + esp_unif);
+console.log("Variance : " + var_unif);
+console.log("Ecart-type : " + ecart_type_bin);
